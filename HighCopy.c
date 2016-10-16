418edf28525abbd6f4871ab04152da2a16d5f826
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-10-16 17:27:23 +0900

    Update HighCopy.c

--- HighCopy.c
+++ HighCopy.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 int main()
 {
 	int nread;
 	FILE *in, *out;
 	int count = 0;
+	int printDot = 0;
 
 	in = fopen("file.in","r");
 	out = fopen("file2.out","w");
@@ -13,9 +13,14 @@ int main()
 	while((nread = fgetc(in)) != EOF){
 		fputc(nread, out);
 		count++;
-		if(count%64 == 0)printf(".");
+		if(count % 1024 == 0){
+			printf(".");
+			printDot++;
+		}
 	}
+
 	printf("\nCopy is Finished\n");
+	printf("DOT : %d", printDot);
 	fclose(in);
 	fclose(out);
 	exit(0);
