b9d00aadd04b16fc7ae2c3613dc115c25d0bfac7
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-11-13 23:07:44 +0900

    Update pmon.c

--- pmon.c
+++ pmon.c
@@ -3,74 +3,81 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <string.h>
 bool isRunning;
-char gpid[10];
-
-void checkFunc(int sig){
-	if(sig == SIGALRM){
-		FILE *fp = NULL;
-		if((fp = popen("pgrep -x ptest", "r")) == NULL)
-			printf("Failed!\n");
-		if(fgets(gpid, 10, fp) != NULL){
-			printf("PID : %s", gpid);
-			isRunning = true;
-			printf("running\n");
-		}
-		else
-			printf("not existed\n");
-		pclose(fp);
+pid_t pid = -1;
+void checkFunc(char* pname){
+	FILE *fp;
+	char buf[1024];
+	memset(buf, '\0', 50);
+	fp = popen("pgrep -x ptest", "r");
+	if(fp == NULL)
+		printf("Failed!\n");
+	fgets(buf, 10, fp);
+	if(buf[0] != '\0'){
+		pid = atoi(buf);
+		printf("running\n");
 	}
+	else{
+		pid = -1;
+		printf("not existed\n");
+	}
+	pclose(fp);
+}
+void Alarm(void)
+{
+	checkFunc("ptest");
+	fflush(stdout);
 	alarm(5);
 }
-
 int main(void)
 {
-	isRunning = false;
 	char message;
-	(void)signal(SIGALRM, checkFunc);
+	(void)signal(SIGALRM, (void*)Alarm);
 	alarm(5);
 	
 	while(1){
 		scanf("%c", &message);
 		switch(message){
 		case 'Q' : case 'q':
-			if(isRunning){
-				printf("Kill ptest program\n");
-				kill(atoi(gpid), SIGKILL);
-				isRunning = false;
+			checkFunc("ptest");
+			if(pid != -1){
+				printf("Quit ptest program\n");
+				kill(pid, SIGKILL);
+				pid = -1;
 			}
 			printf("Quit pmon program\n");
 			exit(1);
 		case 'K' : case 'k':
-			if(isRunning){
+			checkFunc("ptest");
+			if(pid != -1){
 				printf("Kill ptest program\n");
-				kill(atoi(gpid), SIGKILL);
+				kill(pid, SIGKILL);
+				pid = -1;
 			}
 			else
 				printf("ptest is not existed\n");	
-			isRunning = false;
 			break;
 		case 'S' : case 's':
-			if(isRunning)
+			checkFunc("ptest");
+			if(pid != -1)
 				printf("Still running\n");
 			else{
 				printf("Now ptest is running\n");
 				system("gnome-terminal --command \"./ptest\"");
-				isRunning = true;
 			}	
 			break;
 		case 'R' : case 'r':
-			if(isRunning){
+			checkFunc("ptest");
+			if(pid != -1){
 				printf("Re-start\n");
-				kill(atoi(gpid), SIGKILL);
+				kill(pid, SIGKILL);
 				system("gnome-terminal --command \"./ptest\"");
 			}
 			else{
-				printf("Still running\n");
+				printf("Now ptest is running\n");
 				system("gnome-terminal --command \"./ptest\"");
 			}
-			isRunning = true;
 			break;
 		}//switch
 	}// while
